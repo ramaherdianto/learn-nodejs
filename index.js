8eb7322571ac02589a395fbbe8fd333ea0264170
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-02-27 15:01:15 +0700

    Eksport and import module in nodejs

---
+++ index.js
@@ -0,0 +1,11 @@
+const coffeStock = require("./state");
+
+const makeCoffe = (type, miligrams) => {
+  if (coffeStock[type] >= miligrams) {
+    console.log("Kopi berhasil dibuat!");
+  } else {
+    console.log("Biji kopi habis!");
+  }
+};
+
+makeCoffe("arabica", 80);
---
+++ state.js
@@ -0,0 +1,8 @@
+const coffeStock = {
+  arabica: 100,
+  robusta: 150,
+  liberica: 200,
+};
+
+module.exports = coffeStock;
+console.log(module);
